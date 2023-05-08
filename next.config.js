8c2c63d9d8fde787999e1ee74deb607d77a56f5a
oliveraw <oliveraw@umich.edu>
2023-05-08 16:17:52 -0400

    most basic api call working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:5000/:path*',
+        },
+      ]
+    },
+};
