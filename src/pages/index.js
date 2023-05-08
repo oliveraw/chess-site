8387d578063aa10e6565fc667fa3bb798ab71e38
oliveraw <oliveraw@umich.edu>
2023-05-08 19:46:57 -0400

    seems to show basic board

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,47 @@
-import ChessBoard from 'next-chess-board'
-import { useRef } from 'react'
+import { Chessboard } from "react-chessboard";
+import { useState } from "react";
+import { Chess } from "chess.js";
 
-export default () => {
-    let board = <ChessBoard size={400} moveValidator={true}/>
+// function PlayRandomMoveEngine() {
+//   const [game, setGame] = useState(new Chess());
 
-    return (
-        <div class='flex flex-col justify-center items-center'>
-            {board}
+//   function makeAMove(move) {
+//     const gameCopy = { ...game };
+//     const result = gameCopy.move(move);
+//     setGame(gameCopy);
+//     return result; // null if the move was illegal, the move object if the move was legal
+//   }
+
+//   function makeRandomMove() {
+//     const possibleMoves = game.moves();
+//     if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return; // exit if the game is over
+//     const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+//     makeAMove(possibleMoves[randomIndex]);
+//   }
+
+//   function onDrop(sourceSquare, targetSquare) {
+//     const move = makeAMove({
+//       from: sourceSquare,
+//       to: targetSquare,
+//       promotion: "q", // always promote to a queen for example simplicity
+//     });
+
+//     // illegal move
+//     if (move === null) return false;
+//     setTimeout(makeRandomMove, 200);
+//     return true;
+//   }
+
+//   return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
+// }
+
+export default function Home() {
+  return (
+    <div class='flex flex-col justify-center items-center w-full h-full'>
+        <div class='w-1/3 h-1/3 pt-10'>
+            <Chessboard id="BasicBoard" />
+            {/* <PlayRandomMoveEngine/> */}
         </div>
-    )
+    </div>
+  );
 }
